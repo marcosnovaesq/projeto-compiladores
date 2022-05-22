47cba29fd36318d5cdc7c26c03563b29c73ee3c6
marcos <marcosnovaes>
2022-05-22 09:38:48 -0300

    lexicalling

--- base.py
+++ base.py
@@ -1,72 +1,33 @@
-import re
+single_char_token_delimiters = ['+', '-', '*', '/', '<',  '>',  '=', ';', ',', '(', ')', '[',  ']', '{',  '}' ]
+compound_token_delimiters = ['<=','>=', '==', '!=', '/*', '*/']
+compound_first_char = ['<', '>', '=', '!', '/', '*']
 
-example_numerical_token_definition = {
-    'token_type': 'NUMBER',
-    'string_value': '234'
-}
-example_literal_token_definition = {
-    'token_type': 'LITERAL',
-    'string_value': 'joaozinho'
-}
-# add more examples above
-
-
-
-rules = [
-    ('MAIN', r'main'),  # main
-    ('INT', r'int'),  # int
-    ('FLOAT', r'float'),  # float
-    ('IF', r'if'),  # if
-    ('ELSE', r'else'),  # else
-    ('WHILE', r'while'),  # while
-    ('READ', r'read'),  # read
-    ('PRINT', r'print'),  # print
-    ('LBRACKET', r'\('),  # (
-    ('RBRACKET', r'\)'),  # )
-    ('LBRACE', r'\{'),  # {
-    ('RBRACE', r'\}'),  # }
-    ('COMMA', r','),  # ,
-    ('PCOMMA', r';'),  # ;
-    ('EQ', r'=='),  # ==
-    ('NE', r'!='),  # !=
-    ('LE', r'<='),  # <=
-    ('GE', r'>='),  # >=
-    ('OR', r'\|\|'),  # ||
-    ('AND', r'&&'),  # &&
-    ('ATTR', r'\='),  # =
-    ('LT', r'<'),  # <
-    ('GT', r'>'),  # >
-    ('PLUS', r'\+'),  # +
-    ('MINUS', r'-'),  # -
-    ('MULT', r'\*'),  # *
-    ('DIV', r'\/'),  # /
-    ('ID', r'[a-zA-Z]\w*'),  # IDENTIFIERS
-    ('FLOAT_CONST', r'\d(\d)*\.\d(\d)*'),  # FLOAT
-    ('INTEGER_CONST', r'\d(\d)*'),  # INT
-    ('NEWLINE', r'\n'),  # NEW LINE
-    ('SKIP', r'[ \t]+'),  # SPACE and TABS
-    ('MISMATCH', r'.'),  # ANOTHER CHARACTER
-]
-
-
-def lexemize_file():
-    tokens = []
-    with open("example.txt") as f:
+def lexemize_file(filename):
+    lexemes = []
+    with open(filename) as f:
         line = f.readline()
         while line:
             curr_tkn = ""
             has_new_line = False
             for char in line:
+                # empty space 
                 if char == " ":
                     if curr_tkn != "":
-                        tokens.append(curr_tkn)
+                        lexemes.append(curr_tkn)
                     curr_tkn = ""
                     continue
-                # add more token delimiter and maybe array token delimiter
-                elif char == "[" or char == "]" or char == "{" or char == "}" or char == "=" or char == "+" or char == "-" or char == ">" or char == "<":
+                elif char in compound_first_char and curr_tkn == "":
+                    curr_tkn += char
+                    continue
+                elif (curr_tkn + char) in compound_token_delimiters:
+                    curr_tkn += char
+                    lexemes.append(curr_tkn)
+                    curr_tkn = ""
+                    continue
+                elif (curr_tkn + char) not in compound_token_delimiters and char in single_char_token_delimiters:
                     if curr_tkn != "":
-                        tokens.append(curr_tkn)
-                    tokens.append(char)
+                        lexemes.append(curr_tkn)
+                    lexemes.append(char)
                     curr_tkn = ""
                     continue
                 else:
@@ -77,12 +38,17 @@ def lexemize_file():
                     continue
             # end of line processing
             if curr_tkn != "":
-                tokens.append(curr_tkn)
+                lexemes.append(curr_tkn)
             if has_new_line:
-                tokens.append('\n')
+                lexemes.append('\n')
                 has_new_line = False
             line = f.readline()
-    print(tokens)
-    return tokens
+    print(lexemes)
+    return lexemes
 
-lexemize_file()
+
+def tokenize_lexemes(lexemes):
+    print('lexemes')
+
+
+lexemize_file("example.txt")
